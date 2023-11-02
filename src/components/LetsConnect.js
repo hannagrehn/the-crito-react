f25113062dcdcdaca6bebc17ef8d1338a7ee4d26
Hanna Grehn <hannagrehn@gmail.com>
2023-11-02 13:34:22 +0100

    more things

--- src/components/LetsConnect.js
+++ src/components/LetsConnect.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import lines from '../components/images/background-lines.svg'
+import lines from '../components/images/background-lines.svg';
+import { NavLink } from 'react-router-dom';
+
 
 const LetsConnect = () => {
   return (
     <section className="lets-connect">
     <div className="content">
         <div className="small-text">
-            <a href="index.html">Home</a>
-            <a className="active" href="contact.html">Contact</a>
+        <NavLink to="/">Home</NavLink>
+            <a className="active">Contact</a>
         </div>
         <div className="big-text">
             <a><h1>Let's Connect</h1></a>
